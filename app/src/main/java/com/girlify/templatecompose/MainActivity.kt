dd32bf4bf96d0ef3b9c9b019f19d5423b7e95a61
Noelia Carosella <72532638+honeybadger2788@users.noreply.github.com>
2023-05-03 16:38:48 -0300

    mvvm implemented (#2)

--- app/src/main/java/com/girlify/templatecompose/MainActivity.kt
+++ app/src/main/java/com/girlify/templatecompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.girlify.templatecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.girlify.templatecompose.project.ui.Greeting
+import com.girlify.templatecompose.project.ui.GreetingViewModel
 import com.girlify.templatecompose.ui.theme.TemplateComposeTheme
 
 class MainActivity : ComponentActivity() {
+    private val greetingViewModel: GreetingViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +26,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(greetingViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TemplateComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/girlify/templatecompose/project/ui/model/DataModel.kt
@@ -0,0 +1,3 @@
+package com.girlify.templatecompose.project.ui.model
+
+class DataModel (val name: String)
