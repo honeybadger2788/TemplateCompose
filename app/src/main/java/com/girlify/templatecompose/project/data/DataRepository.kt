ce6d14f64169f675ae0b13289946765a2eeaf991
Noelia Carosella <72532638+honeybadger2788@users.noreply.github.com>
2023-05-04 17:03:40 -0300

    di implemented (#3)

--- app/src/main/java/com/girlify/templatecompose/project/data/DataRepository.kt
+++ app/src/main/java/com/girlify/templatecompose/project/data/DataRepository.kt
@@ -1,8 +1,9 @@
 package com.girlify.templatecompose.project.data
 
 import com.girlify.templatecompose.project.ui.model.DataModel
+import javax.inject.Inject
 
-class DataRepository {
+class DataRepository @Inject constructor() {
     private val dataProvider = DataProvider
 
     fun getData(): List<DataModel> =
--- app/src/main/java/com/girlify/templatecompose/project/domain/DataUseCase.kt
+++ app/src/main/java/com/girlify/templatecompose/project/domain/DataUseCase.kt
@@ -2,9 +2,10 @@ package com.girlify.templatecompose.project.domain
 
 import com.girlify.templatecompose.project.data.DataRepository
 import com.girlify.templatecompose.project.ui.model.DataModel
+import javax.inject.Inject
 
-class DataUseCase {
-    private val dataRepository: DataRepository = DataRepository()
-
+class DataUseCase @Inject constructor(
+    private val dataRepository: DataRepository
+) {
     operator fun invoke(): DataModel = dataRepository.getData().random()
 }
